d5462987630655c6c47dc7ed727faee138674af8
Eduardo Cristian Campigoto <ecc.dvlopment@gmail.com>
2017-08-24 20:38:01 -0300

    Removida a pasta target do maven

---
+++ ong-cepe/src/br/com/cepe/entity/pojo/pessoa/DoadorPf.java
@@ -0,0 +1,11 @@
+/**
+ * @author  Eduardo Cristian Campigoto
+ **/
+package br.com.cepe.entity.pojo.pessoa;
+
+import javax.persistence.DiscriminatorValue;
+
+@DiscriminatorValue(value = "5")
+public class DoadorPf extends PessoaFisica{
+
+}

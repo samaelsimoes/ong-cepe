43960fdd90ba1e08d6e79cdfc1a69930d624e050
Eduardo Cristian Campigoto <ecc.dvlopment@gmail.com>
2017-08-28 19:31:22 -0300

    ...

--- ong-cepe/src/br/com/cepe/rest/PessoaRest.java
+++ ong-cepe/src/br/com/cepe/rest/PessoaRest.java
@@ -2,6 +2,7 @@
  * @author  Eduardo Cristian Campigoto
  **/
 package br.com.cepe.rest;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -19,73 +20,65 @@ import br.com.cepe.factory.util.ObjMapper;
 import br.com.cepe.service.PessoaService;
 
 @Path("/pessoa")
-public class PessoaRest extends ObjMapper{
-private PessoaService<?> pessoaService;
+public class PessoaRest extends ObjMapper {
+	private PessoaService<?> pessoaService;
 
-public PessoaRest(){
+	public PessoaRest() {
 
-}
-
-
-/**
- * @param pessoaStr - Json da entidade pessoa.
- * **/
-@POST
-@Consumes("application/*")
-public void adicionar(String pessoaStr) throws GlobalException{
-	try {
-	Pessoa obj = getObject().readValue(pessoaStr, Pessoa.class);
-	Pessoa pessoa = (Pessoa) new PessoaFactory<Pessoa>(obj).getPessoa();
-	pessoaService = new PessoaService<Pessoa>(pessoa);
-	pessoaService.adicionar();
-		
-	} catch (Throwable e) {
-		e.printStackTrace();
-	}
-	
-} 
-
-@GET
-<<<<<<< HEAD
-@PathParam("/nome/{nome}")
-@Produces({MediaType.APPLICATION_JSON})
-public String pesquisarNome(@PathParam("nome") String nome){
-	try {
-=======
-@Path("/pessoa/{nome}")
-@Produces({MediaType.APPLICATION_JSON})
-public StringBuilder pesquisarNome(@PathParam("nome")String nome){
-	System.out.println("teste" + nome);
-
-	try {
-		
->>>>>>> 36aad5a92d348bbd1acff2e5f237c6a52fd50dea
-		return getJson(pessoaService.pesquisarNome(nome));
-		
-	} catch (Throwable e) {
-		e.printStackTrace();
 	}
 
-}
+	/**
+	 * @param pessoaStr
+	 *            - Json da entidade pessoa.
+	 * **/
+	@POST
+	@Consumes("application/*")
+	public void adicionar(String pessoaStr) throws GlobalException {
+		try {
+			Pessoa obj = getObject().readValue(pessoaStr, Pessoa.class);
+			Pessoa pessoa = (Pessoa) new PessoaFactory<Pessoa>(obj).getPessoa();
+			pessoaService = new PessoaService<Pessoa>(pessoa);
+			pessoaService.adicionar();
 
-@PUT
-@Consumes("application/*")
-public void alterar(String pessoaStr) throws GlobalException{
-	try {
-		Pessoa obj = getObject().readValue(pessoaStr, Pessoa.class);
-		Pessoa pessoa = (Pessoa) new PessoaFactory<Pessoa>(obj).getPessoa();
-		pessoaService = new PessoaService<Pessoa>(pessoa);
-		pessoaService.alterar();
-	} catch (Throwable e) {
-		e.printStackTrace();
-	} 
-}
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
 
-@DELETE
-@PathParam("id")
-public void excluir(@PathParam("id") int id){
-	pessoaService.excluir(id);
-}
+	}
 
+	@GET
+	@PathParam("/nome/{nome}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String pesquisarNome(@PathParam("nome") String nome) {
+
+		try {
+
+			return getJson(pessoaService.pesquisarNome(nome));
+
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@PUT
+	@Consumes("application/*")
+	public void alterar(String pessoaStr) throws GlobalException {
+		try {
+			Pessoa obj = getObject().readValue(pessoaStr, Pessoa.class);
+			Pessoa pessoa = (Pessoa) new PessoaFactory<Pessoa>(obj).getPessoa();
+			pessoaService = new PessoaService<Pessoa>(pessoa);
+			pessoaService.alterar();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	@DELETE
+	@PathParam("id")
+	public void excluir(@PathParam("id") int id) {
+		pessoaService.excluir(id);
+	}
 
 }

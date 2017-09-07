92d7fe6b581baa8532e0bbad8d8cd005770b916e
Eduardo Cristian Campigoto <ecc.dvlopment@gmail.com>
2017-09-06 21:33:21 -0300

    Correção busca

--- ong-cepe/src/br/com/cepe/factory/entity/pessoa/PessoaFactory.java
+++ ong-cepe/src/br/com/cepe/factory/entity/pessoa/PessoaFactory.java
@@ -20,19 +20,6 @@ import br.com.cepe.factory.util.ObjMapper;
 
 public class PessoaFactory extends ObjMapper {
 	private List<Pessoa> pessoas = new ArrayList<Pessoa>();
-<<<<<<< HEAD
-
-	/**
-	 * Retorna objeto Pessoa de acordo com o tipo passado por parametro, podendo
-	 * ser pessoa física ou jurídica
-	 * 
-	 * @param tipo
-	 * @return Pessoa
-	 */
-
-	public PessoaFactory() {
-
-=======
 	private Class<?> pessoaClasse;
 	private ObjectNode objNode;
 	
@@ -46,7 +33,6 @@ public class PessoaFactory extends ObjMapper {
 		PESSOA_CLASSES.put(5, Patrocinador.class);
 		PESSOA_CLASSES.put(6, Beneficiario.class);
 		PESSOA_CLASSES.put(7, Atleta.class);
->>>>>>> a8157f5e48da82cffed964c4e8fd53f9e49e8146
 	}
 	
 
@@ -56,11 +42,6 @@ public class PessoaFactory extends ObjMapper {
 		try {
 			
 			objNode = getObject().readValue(pessoaStr, ObjectNode.class);
-<<<<<<< HEAD
-			
-=======
-
->>>>>>> a8157f5e48da82cffed964c4e8fd53f9e49e8146
 			if (objNode != null)
 				tipo = objNode.get("tipo").asInt();
 			else
@@ -68,62 +49,12 @@ public class PessoaFactory extends ObjMapper {
 						"Falha ao receber o atributo tipo de Pessoa");
 
 			if (tipo != 0) {
-<<<<<<< HEAD
 				
-				//type = 1 
-				if (tipo == PessoaType.PF.getIndex())
-					classe = PessoaFisica.class;
-				
-				//type = 2
-				if (tipo == PessoaType.PJ.getIndex())
-					classe = PessoaJuridica.class;
-				
-				//type = 3
-				if (tipo == PessoaType.DOADOR_PF.getIndex())
-					classe = DoadorPf.class;
-				
-				//type = 4
-				if (tipo == PessoaType.DOADOR_PJ.getIndex())
-					classe = DoadorPj.class;
-				
-				// type = 5
-				//if (tipo == PessoaType.PATROCIN.getIndex())
-
-				else if (tipo == PessoaType.PJ.getIndex())
-					classe = PessoaJuridica.class;
-
-				else if (tipo == PessoaType.BENEFIC.getIndex())
-					classe = Beneficiario.class;
-
-				else if (tipo == PessoaType.DOADOR_PF.getIndex())
-					classe = DoadorPf.class;
-
-				else if (tipo == PessoaType.DOADOR_PJ.getIndex())
-					classe = DoadorPj.class;
-
-				else if (tipo == PessoaType.ATLETA.getIndex())
-					classe = Atleta.class;
-
-				else if (tipo == PessoaType.PATROCIN.getIndex())
-					classe = Patrocinador.class;
-				
-				//type = 6
-				if (tipo == PessoaType.BENEFIC.getIndex())
-					classe = Beneficiario.class;
-				
-				// type 7
-				if (tipo == PessoaType.ATLETA.getIndex())
-					classe = Atleta.class;
-				
-				if (classe != null) {
-					Pessoa obj = (Pessoa) getObject().readValue(pessoaStr, classe);
-=======
 				this.pessoaClasse = PESSOA_CLASSES.get(tipo);
 
 				if (this.pessoaClasse != null) {
 					Pessoa obj = (Pessoa) getObject().readValue(pessoaStr,
 							this.pessoaClasse);
->>>>>>> a8157f5e48da82cffed964c4e8fd53f9e49e8146
 					this.pessoas.add(obj);
 				}
 			}
@@ -133,7 +64,7 @@ public class PessoaFactory extends ObjMapper {
 			throw new GlobalException("Erro de factory na classe Pessoa");
 		}
 	}
-
+	
 
 	public PessoaFactory(int tipo){
 		this.pessoaClasse = PESSOA_CLASSES.get(tipo);

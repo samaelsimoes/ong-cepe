56fb23810d7503873d492a7d95898d84c77b1e4b
Samael Pereira Simoes <samael.simoes@gmail.com>
2017-09-18 20:33:13 -0300

    commit

--- ong-cepe/src/br/com/cepe/rest/EstadoRest.java
+++ ong-cepe/src/br/com/cepe/rest/EstadoRest.java
@@ -26,8 +26,8 @@ public class EstadoRest extends ObjMapper {
 		try {
 			
 			List<Estado> estados = new EstadoService().pesquisaEstado();
-
 			String resp = getJson(estados);
+			
 			return Response.ok(resp ,MediaType.APPLICATION_JSON).build();
 		} catch (Throwable e) {
 			e.printStackTrace();

ca5a2f2cb0ce6b5e318dfb77ed5f919c378ba530
Samael Pereira Simoes <samael.simoes@gmail.com>
2017-10-03 22:06:33 -0300

    ajustes evento

---
+++ ong-cepe/WebContent/project/js/eventos/chamadas.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+	$('#header').load('header.html');
+	$('#footer').load('footer.html');
+	$('#eventos').load('formulario/eventos/searchAll/gridEvento.html');
+});
+
+/*var items = document.querySelector('#tipoevento');
+
+items.addEventListener('change', function(){
+	
+	var valor =	this.value // o valor que procuras é: this.value
+
+	if(valor == 0) {	
+		
+		$('#eventos').load('formulario/eventos/gridEvento');
+	}/* else if(valor == 1 ) {
+		
+		$('#eventos').load('formulario/eventos/');
+	}else if( valor == 2){
+		$('#eventos').load('formulario/eventos/');
+	}
+});*/
+

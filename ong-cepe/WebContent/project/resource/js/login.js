f0b3e0c0b2fa34945a3c4493274e1751639d9117
Samael Pereira Simoes <samael.simoes@gmail.com>
2017-11-07 22:03:02 -0200

    ajustes layout front end modal

--- ong-cepe/WebContent/project/resource/js/login.js
+++ ong-cepe/WebContent/project/resource/js/login.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 			var criptbase64=btoa($("#senha").val());
 			$("#passwordhidden").val(criptbase64);
 			
-			msg += ONG.login.valida("Campo Senha ", $("#passwordhidden").val());
+			//msg += ONG.login.valida("Campo Senha ", $("#passwordhidden").val()); //verificando a senha que esta sendo salva em um ID HIDDEN no index.html
 			
 			if(msg == "") {
 				$.ajax({	
